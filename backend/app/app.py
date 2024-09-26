948cabe2a4dfc503051da49dc32e639b47a38313
P3STEBUBONICAKK <jorjinhodopneugominhos@gmail.com>
2024-09-26 12:15:38 -0300

    update app.py
    Finalização do servidor flask

--- backend/app/app.py
+++ backend/app/app.py
@@ -13,5 +13,8 @@ logging.basicConfig(level=logging.DEBUG)
 
 @app.route('/')
 def index():
-    return render_template('../frontend/templates/index.html')
+    return render_template('../frontend/templates/login.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
 
