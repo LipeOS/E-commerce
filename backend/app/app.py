e7c9d995f8a0ea2ba9e0e51b00921f4855dcc74a
Felipe Silva <131161088+LipeOS@users.noreply.github.com>
2024-09-26 20:13:12 -0300

    Correcao e revisamento
    Coisas simples

--- backend/app/app.py
+++ backend/app/app.py
@@ -1,8 +1,11 @@
 from flask import Flask, render_template, request, redirect, url_for, jsonify
 from flask_mysqldb import MySQL
 import logging
+import os
 
-app = Flask (__name__)
+template_dir = os.path.join(os.getcwd(), 'frontend')
+
+app = Flask(__name__, template_folder=template_dir)
 
 app.config['MYSQL_USER'] = 'root'
 app.config['MYSQL_PASSWORD'] = '1234'
@@ -13,8 +16,7 @@ logging.basicConfig(level=logging.DEBUG)
 
 @app.route('/')
 def index():
-    return render_template('../frontend/templates/login.html')
+    return render_template('../../frontend/templates/login.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
-
