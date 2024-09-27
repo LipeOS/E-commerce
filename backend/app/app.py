e3efc86df7bc1c441ba143afadd5a89ac93da860
P3STEKK <015998239717cida@gmail.com>
2024-09-27 10:29:13 -0300

    update app.py
    Algumas mudanças rapidas de codigo

--- backend/app/app.py
+++ backend/app/app.py
@@ -1,22 +1,11 @@
 from flask import Flask, render_template, request, redirect, url_for, jsonify
 from flask_mysqldb import MySQL
 import logging
-import os
 
-template_dir = os.path.join(os.getcwd(), 'frontend')
-
-app = Flask(__name__, template_folder=template_dir)
-
-app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = '1234'
-app.config['MYSQL_DB'] = 'banco'
-app.config['MYSQL_HOST'] = 'localhost'  
-
-logging.basicConfig(level=logging.DEBUG)
-
+app = Flask(__name__)
 @app.route('/')
-def login():
-    return render_template('../../frontend/templates/login.html')
+def index():
+    return render_template('cadastro.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
